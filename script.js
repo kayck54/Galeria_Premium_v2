a118430a7fb5eb889862dd456781c713dee3eecc
kayck54 <kayckantonio1@gmail.com>
2026-07-30 08:23:44 -0300

    Fix syntax error in imagens array declaration

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ import {
 // ==============================
 
 const imagens = [
-     id: "anime",
+    {  id: "anime",
         titulo: "Dragon Ball Super",
         descricao: "Sayajin fortão igual o @kayck.01_",
         categoria: "Anime",
